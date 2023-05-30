499e8c8594a5cb70202fdb5da287cffef8d6fae8
nicollypatriciag <130609908+nicollypatriciag@users.noreply.github.com>
2023-05-29 21:30:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname+'/'));
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(3000,function(erro){
+    
+    if(erro){
+        console.log("Erro ao iniciar!");
+    }else{
+        console.log("Servidor iniciado!");
+    }
+})
